75e940087dd60fb89360250496dc4f9922402645
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2020-01-17 09:19:57 +0900

    [UPDATE] unity version up

---
+++ Assets/Plugins/UH.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Sequential)]
+struct UHSimpleInterface
+{
+	public bool valid;
+
+	public double qw;
+	public double qx;
+	public double qy;
+	public double qz;
+
+	public double pr0;
+	public double pr1;
+	public double pr2;
+	public double pr3;
+	public double pr4;
+	public double pr5;
+	public double pr6;
+	public double pr7;
+
+	public double temp;
+
+	public float last_computed_frequency;
+}
+
+public static class UH {
+	
+	public static int CONTINUOUS_MODE_QUATERNION       = 0x01;
+	public static int CONTINUOUS_MODE_RAW_ACCELERATION = 0x02;
+	public static int CONTINUOUS_MODE_RAW_GYROMETER    = 0x04;
+	public static int CONTINUOUS_MODE_PHOTOREFLECTOR   = 0x08;
+	public static int CONTINUOUS_MODE_TEMPERATURE      = 0x10;
+
+	[DllImport("libUHAPI")]
+	private static extern void openUH (string serialport, int flags); 
+
+	[DllImport("libUHAPI")]
+	private static extern void closeUH (string serialport); 
+
+	[DllImport("libUHAPI")]
+	private static extern UHSimpleInterface readUH (string serialport); 
+
+	[DllImport("libUHAPI")]
+	private static extern bool existsUH(string serialport); 
+
+	[DllImport("libUHAPI")]
+	private static extern void stimulateChannelUH(string serial_port, int channel);
+
+	[DllImport("libUHAPI")]
+	private static extern void increaseStimulationTimeUH(string serial_port);
+
+	[DllImport("libUHAPI")]
+	private static extern void decreaseStimulationTimeUH(string serial_port);
+
+	[DllImport("libUHAPI")]
+	private static extern void increaseStimulationVoltageUH(string serial_port);
+
+	[DllImport("libUHAPI")]
+	private static extern void decreaseStimulationVoltageUH(string serial_port);
+
+	public static void open(string serialport, int flags)
+	{
+		openUH (serialport, flags);
+	}
+
+	public static void close(string serialport)
+	{
+		closeUH (serialport);
+	}
+
+	public static bool exists(string serialport)
+	{
+		return existsUH (serialport);
+	}
+
+	public static Quaternion rotation(string serialport)
+	{
+		UHSimpleInterface outputinfo = readUH (serialport);
+		Quaternion Rot;
+		Rot.w = (float)outputinfo.qw;
+		Rot.x = (-1) * (float)outputinfo.qx;
+		Rot.y = (-1) * (float)outputinfo.qz;
+		Rot.z = (-1) * (float)outputinfo.qy;
+		return Rot;
+	}
+
+	public static double[] photoReflectors(string serialport)
+	{
+		UHSimpleInterface outputinfo = readUH (serialport);
+		double[] return_value = new double[8];
+		return_value [0] = outputinfo.pr0;
+		return_value [1] = outputinfo.pr1;
+		return_value [2] = outputinfo.pr2;
+		return_value [3] = outputinfo.pr3;
+		return_value [4] = outputinfo.pr4;
+		return_value [5] = outputinfo.pr5;
+		return_value [6] = outputinfo.pr6;
+		return_value [7] = outputinfo.pr7;
+		return return_value;
+	}
+
+	public static void stimulateChannel(string serialport, int channel)
+	{
+		stimulateChannelUH (serialport, channel);
+	}
+
+	public static void increaseStimulationTime(string serialport)
+	{
+		increaseStimulationTimeUH (serialport);
+	}
+
+	public static void decreaseStimulationTime(string serialport)
+	{
+		decreaseStimulationTimeUH (serialport);
+	}
+
+	public static void increaseStimulationVoltage(string serialport)
+	{
+		increaseStimulationVoltageUH (serialport);
+	}
+
+	public static void decreaseStimulationVoltage(string serialport)
+	{
+		decreaseStimulationVoltageUH (serialport);
+	}
+
+}
---
+++ Assets/Plugins/UHController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class UHController : MonoBehaviour {
+
+	public string serial_port_name = "/dev/cu.usbserial-A104X0TS";
+
+	void Start()
+	{
+		print ("Opening UH");
+		int flags = UH.CONTINUOUS_MODE_QUATERNION | UH.CONTINUOUS_MODE_PHOTOREFLECTOR;
+		UH.open(serial_port_name,flags);
+
+	}
+
+	void Update()
+	{
+		//print ("Reading from UH...");
+
+		if (UH.exists(serial_port_name)) {
+
+			//Set object rotation to match UHs rotation
+			transform.rotation = UH.rotation(serial_port_name);
+			print ("Rotation UH...");
+
+			//Set color from Photo Reflector values
+			double[] photo_reflectors = UH.photoReflectors(serial_port_name);
+			Vector3 color_vec;
+			color_vec.x = (float)photo_reflectors[0];
+			color_vec.y = (float)photo_reflectors[1];
+			color_vec.z = (float)photo_reflectors[2];
+			color_vec.Normalize ();
+			Color main_color = Color.red;
+			main_color.r = color_vec.x;
+			main_color.g = color_vec.y;
+			main_color.b = color_vec.z;
+			main_color.a = 1;
+			Renderer rend = GetComponent<Renderer> ();
+			rend.material.shader = Shader.Find ("Specular");
+			rend.material.SetColor ("_Color", main_color);
+
+
+			if (Input.GetKeyDown (KeyCode.Alpha0)) {
+				UH.stimulateChannel (serial_port_name, 0);
+			}else if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				UH.stimulateChannel (serial_port_name, 1);
+			}else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+				UH.stimulateChannel (serial_port_name, 2);
+			}else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+				UH.stimulateChannel (serial_port_name, 3);
+			}else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+				UH.stimulateChannel (serial_port_name, 4);
+			}else if (Input.GetKeyDown (KeyCode.Alpha5)) {
+				UH.stimulateChannel (serial_port_name, 5);
+			}else if (Input.GetKeyDown (KeyCode.Alpha6)) {
+				UH.stimulateChannel (serial_port_name, 6);
+			}else if (Input.GetKeyDown (KeyCode.Alpha7)) {
+				UH.stimulateChannel (serial_port_name, 7);
+			}
+
+
+		}
+
+	}
+
+	void OnDestroy()
+	{
+		print ("Closing UH");
+		UH.close (serial_port_name);
+	}
+
+}
